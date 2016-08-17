0b562fd633922d4fec65df8e9a82557688792794
nhaglind <nhaglind@gmail.com>
2016-08-17 13:05:28 -0500

    init push

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function additionalUser() {
+  var inputField = document.createElement("p");
+  var addField = document.createElement("input");
+  inputField.appendChild(addField);
+  document.getElementById("userField").appendChild(inputField);
+}
+
+function removeUser() {
+}
